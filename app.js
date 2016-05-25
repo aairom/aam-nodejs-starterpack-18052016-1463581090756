97d296f512498707a97c4a7c0fb4feaee7a3d565
Alain AIROM <alain.airom@fr.ibm.com>
2016-05-25 09:39:36 -0500

    app.js échec

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ done(null, obj);
 // VCAP_SERVICES contains all the credentials of services bound to
 // this application. For details of its content, please refer to
 // the document or sample of each service.
+ /*
 var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
 var ssoConfig = services.SingleSignOn[0];
 var client_id = ssoConfig.credentials.clientId;
@@ -80,7 +81,7 @@ var Strategy = new OpenIDConnectStrategy({
         callbackURL : callback_url,
         skipUserProfile: true,
         issuer: issuer_id},
- /*
+
  function(accessToken, refreshToken, profile, done) {
         process.nextTick(function() {
         profile.accessToken = accessToken;
